e644d36d0aea0405427fa7981fcd61bb0cb10d0f
Jordan Parker <6415727+byjpr@users.noreply.github.com>
2025-08-03 13:42:49 +0100

    chore(release): bump version from 0.1.0 to 0.2.0

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Pike.Mixfile do
     [
       app: :pike,
       description: "Guard at the API Gate",
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.6",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
